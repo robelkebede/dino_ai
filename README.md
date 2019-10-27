bb2414a88a49130af5f63a86cd76be5e0378e671
INC SERVER1 <robelkebede44@gmai.com>
2019-10-27 14:49:26 +0300

    README updated

--- README.md
+++ README.md
@@ -1,5 +1,39 @@
-# dino_ai
+# Dino_ai
 play chrome Dinosaur
 
-* Faild to detect small Trees
-* the models worked 
+* Can play
+* Faild to detect small Trees 
+* Multi layter Preptron
+* model 87% Accuracy
+
+
+## Installation
+
+```bash 
+    git clone https://github.com/robelkebede44/dino_ai.git
+    pip install -r requirements.txt 
+```
+
+## Usage
+
+* Open google Chrome Browser 
+* URL  chrome://dino
+
+
+``` bash 
+    cd ./dino_ai
+    python test.py 
+```
+
+## Video
+
+
+## Contributing
+
+Pull request are welcome
+
+## Licence
+
+[MIT] (https://choosealicense.com/licenses/mit/)
+
+
--- train.py
+++ train.py
@@ -9,64 +9,30 @@ from sklearn.neural_network import MLPClassifier
 import pandas as pd
 import joblib
 
+data = np.load("./dataset/train_03_resize.npy",allow_pickle=True)
 
-def old_model():
-    """
-    dataframe = pd.read_csv("dataset_new_4.csv")
-
-    x = dataframe.drop(["label"],axis=1)
-    y = dataframe["label"]   """
-
-    data = np.load("./dataset/train_03_resize.npy",allow_pickle=True)
-
-    x = data[:,0]
-    y = data[:,1]
-
-
-    x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,shuffle=True,random_state=4)
-
-    model = SVC()
-
-    model.fit(x_train,y_train)
-
-    joblib.dump(model,"dino_v6_mss")
-
-    predict = model.predict(x_test)
-
-    print(metrics.classification_report(predict,y_test)) 
-
-
-
-def new_model():
-    data = np.load("./dataset/train_03_resize.npy",allow_pickle=True)
+def model(data):
     x = [cv2.resize(img,(50,50)) for img in data[:,0]]
     y = [tar for tar in data[:,1]]
-    new_y = []
-    """
     
-    for i in y:
-        if i==1:
-            new_y.append([1,0])
-        else:
-            new_y.append([0,1]) """
-
     x = np.array([img for img in x]).reshape(432,50*50)
     y= np.array([tar[0] for tar in y]) 
 
-    print(x[0].shape)
-    print(y[0])
-
     x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,shuffle=True,random_state=4)
 
-
-    network = MLPClassifier(verbose=True)
+    network = MLPClassifier(hidden_layer_sizes=(100,),verbose=True)
 
     network.fit(x_train,y_train)
 
-    #joblib.dump(network,"dino_neural_v1")
+    #joblib.dump(network,"./model/dino_neural_v2")
 
     predict = network.predict(x_test)
 
     print(metrics.classification_report(predict,y_test)) 
 
-new_model()
+
+
+if __name__ == "__main__":
+    model(data)
+
+
